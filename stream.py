b376d7c49f832edbfc47f49d80f45424aa07e9fe
sudarsanpalavalasa <sudharshan@spotmies.ai>
2025-01-03 17:12:43 +0530

    Create stream.py

---
+++ stream.py
@@ -0,0 +1,23 @@
+import cv2
+import cvlib as cv
+from cvlib.object_detection import draw_bbox
+from gtts import gTTS
+from playsound import playsound
+
+video = cv2.VideoCapture(0)
+labels = []
+
+while True:
+    ret, frame = video.read()
+    bbox, label, conf = cv.detect_common_objects(frame)
+    output_image = draw_bbox(frame, bbox, label, conf)
+
+    cv2.imshow("Object Detection", output_image)
+    for item in label:
+        if item in labels:
+            pass
+        else:
+            labels.append(item)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+print(labels)
